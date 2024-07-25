c883b8e2433bdd4e4c5a713002c101ee9550e48e
Maria Eduarda <eduardacampos652@gmail.com>
2024-07-25 18:26:08 -0300

    fist commit

---
+++ BackEnd/src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const routes = express.Router();
+const users = [{
+    id: 1,
+    name: 'Duda',
+    cpf: '0000000000',
+    password: '123456'
+}]
+
+routes.post('/', (request, response)=>{
+    const { cpf, password } = request.body;
+    
+    const user = users.find(user => user.cpf === cpf && user.password === password)
+
+    //validacao
+    if(user){
+        return response.status(200).json()
+    }
+    
+    return response.status(401).json({message: 'Usuário ou senha incorretos'})
+});
+
+module.exports = routes;
